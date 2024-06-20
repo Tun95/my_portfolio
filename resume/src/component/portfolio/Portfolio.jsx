213d94f03e3471409cd564d34d96645d090de5d1
Ola Josh <akandetunji3@gmail.com>
2024-06-20 15:35:32 +0100

    new

--- resume/src/component/portfolio/Portfolio.jsx
+++ resume/src/component/portfolio/Portfolio.jsx
@@ -16,6 +16,7 @@ function Portfolio() {
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+      
       <div className="container portfolio_container">
         <article className="portfolio_item">
           <div className="portfolio_item-image">
